aae2f1f18c44a89b34c6bc9220871f994c0bf75f
donnymays <dtmays2@gmail.com>
2020-12-20 21:06:35 -0800

    create loading spinner on plants list screen

--- screens/PlantsListScreen.js
+++ screens/PlantsListScreen.js
@@ -1,19 +1,47 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { StyleSheet, Text, View, FlatList } from 'react-native';
+import { StyleSheet, Text, View, FlatList, ActivityIndicator } from 'react-native';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 import HeaderButton from '../components/HeaderButton';
 import PlantList from '../components/PlantList'
 import * as plantsActions from '../store/actions/plants';
+import Colors from '../constants/Colors';
+import { DefaultText } from '../components/Text';
 
 const PlantsListScreen = props => {  
+  const [isLoading, setIsLoading] = useState(false)
   const plants = useSelector(state => state.plants.plants);
   const dispatch = useDispatch();
+
+useEffect(() => {
+  const loadPlants = async () => {
+    setIsLoading(true);
+    await dispatch(plantsActions.fetchProducts());
+    setIsLoading(false);
+  };
+  loadPlants();
+}, [dispatch]);
 
   useEffect(() => {
     dispatch(plantsActions.fetchPlants());
   }, [dispatch]);
    
+  if (isLoading) {
+    return(
+      <View style={styles.centered}>
+        <ActivityIndicator size='large' color={Colors.green} />
+      </View>
+    )
+  }
+
+  if (!isLoading && plants.length === 0) {
+    return (
+      <View style={styles.centered}>
+      <DefaultText>No Plants found.  Try adding some.</DefaultText>
+    </View>
+    )
+  }
+
   return (
       <PlantList navigation={props.navigation} listData={plants}/> 
   );
@@ -38,4 +66,10 @@ PlantsListScreen.navigationOptions = navData => {
 
 export default PlantsListScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  centered: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+})
